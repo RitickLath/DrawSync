c85201a4bc5c6e3eea7f558cde332aacb4c538b6
RitickLath <ritick943lath@gmail.com>
2025-03-03 22:40:44 +0530

    db Package | Prisma Init | User, Room, Chat Model | db Package Export & Import | Import Export Issue solved by Upgrading the version to Node 23

--- apps/htttp-server/src/index.ts
+++ apps/htttp-server/src/index.ts
@@ -1,5 +1,25 @@
 import express from "express";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
-app.listen(3001);
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  await prismaClient.user.create({
+    data: {
+      email: "testing",
+      password: "testing",
+      name: "testing",
+    },
+  });
+  res.send("Successful");
+});
+
+app.post("/signin", (req, res) => {});
+
+app.post("/chat-room", (req, res) => {});
+
+app.listen(3001, () => {
+  console.log("Server running");
+});
